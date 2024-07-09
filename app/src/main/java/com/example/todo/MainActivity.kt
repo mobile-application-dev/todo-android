36388c3c9ff7c60c75a1639da5738eb17f661a61
Santiago Huanacu Sanchez <santihs.sanchez.dev@gmail.com>
2024-07-08 23:57:17 -0400

    [@santihsdev] Implement Create Note Fragment with functionality to create Notes

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.todo.view.NoteViewModel
+import com.example.todo.view.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val factory = NoteViewModelFactory()
+        viewModel = ViewModelProvider(this, factory).get(NoteViewModel::class.java)
+
     }
 }
